e56dc140ff644976c30ca1362bad0b283ec0d851
MIINII <ft.dawn.o@gmail.com>
2023-01-26 13:37:29 +0900

    유저 검색페이지 디자인

--- src/components/mainpage/MainPlaylist.tsx
+++ src/components/mainpage/MainPlaylist.tsx
@@ -11,22 +11,29 @@ import { FaHeart } from "react-icons/fa";
 const MainPlayList = () => {
   return (
     <main className="w-full h-48 drop-shadow-sm mb-11">
-      <header className="flex items-center">
-        <figure className="profileImg mr-spacing_xs">
-          <img src={ProfileImg} alt="프로필" width="45px" />
-        </figure>
-        <div className="mr-spacing_xl">
-          <h2 className="text-base text-mpl_black font-medium">여름에 듣기 좋은 노래</h2>
-          <span className="text-sm text-mpl_darkgray">레몬사탕</span>
+      <header className="flex items-center justify-between">
+        <div className="profile flex">
+          <figure className="profileImg mr-spacing_xs">
+            <img src={ProfileImg} alt="프로필" width="45px" />
+          </figure>
+
+          <div className="mr-spacing_xl">
+            <h2 className="text-base text-mpl_black font-medium">여름에 듣기 좋은 노래</h2>
+            <span className="text-sm text-mpl_darkgray">레몬사탕</span>
+          </div>
         </div>
-        <span className="text-sm mr-spacing_sm">총 22곡</span>
-        <span className="text-sm flex items-center">
-          <FaHeart className="text-mpl_pink" />
-          10
-        </span>
+
+        <div className="flex">
+          <span className="text-sm mr-spacing_sm">총 22곡</span>
+          <span className="text-sm flex items-center">
+            <FaHeart className="text-mpl_pink" />
+            10
+          </span>
+        </div>
+
       </header>
       <section className="relative mt-spacing_sm h-32 overflow-hidden">
-        <img src={plyImg} alt="플레이리스트 썸네일" className="absolute -top-1/2 " />
+        <img src={plyImg} alt="플레이리스트 썸네일" className="absolute -bottom-1/2 " />
         <button className="absolute z-10 top-[90px] left-2 opacity-90 ">
           <img src={plybutton} alt="플레이 버튼" />
         </button>
--- src/pages/Searchuserpage.tsx
+++ src/pages/Searchuserpage.tsx
@@ -1,5 +1,46 @@
-const Searchuserpage=()=>{
-    return<></>
+// img
+import ProfileImg from "../assets/basic_profile.svg";
+
+// icon
+import { FaHeart } from "react-icons/fa";
+
+const Searchuserpage = () => {
+  return (
+    <div className="px-10 pt-14">
+      <input
+        id="user_search_input"
+        type="search"
+        className="w-full h-11 p-3 mb-11 border-1 border-indigo-800 bg-mpl_white rounded-full drop-shadow-md"
+        placeholder="😀 친구를 찾아보세요 ㅋ"
+      />
+
+      <div className="profile flex justify-start">
+        <figure className="profileImg mr-spacing_sm">
+          <img src={ProfileImg} alt="프로필" width="45px" />
+        </figure>
+
+        <div className="w-full">
+          <div className="flex">
+            <h2 className="text-base text-mpl_black font-medium">레몬사탕</h2>
+            <span className="flex items-center text-mpl_darkgray mx-2">
+              <FaHeart className="text-mpl_pink mr-1" />
+              <span>30</span>
+            </span>
+          </div>
+
+          <div className="w-full text-sm text-mpl_darkgray flex items-center justify-start">
+            <span className="w-48 block text-ellipsis whitespace-nowrap overflow-hidden">
+              너의 밤이 또 가기전에 내게 말해줘 
+            </span>
+          </div>
+        </div>
+
+        <button className="w-1/4 p-1 bg-mpl_black text-mpl_white text-sm rounded-xl">
+          팔로우
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Searchuserpage;
